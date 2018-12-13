1325e67a8a5a4b99cb64ddf47abddb9a7559c6cc
bonbon <seanmerrifield@gmail.com>
2018-12-13 21:06:59 +0100

    Added Image Caption project

---
+++ image_caption/dataloader.py
@@ -0,0 +1,41 @@
+import os
+import sys
+sys.path.append('/opt/cocoapi/PythonAPI')
+from pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+
+# initialize COCO API for instance annotations
+dataDir = '/opt/cocoapi'
+dataType = 'val2014'
+instances_annFile = os.path.join(dataDir, 'annotations/instances_{}.json'.format(dataType))
+coco = COCO(instances_annFile)
+
+# initialize COCO API for caption annotations
+captions_annFile = os.path.join(dataDir, 'annotations/captions_{}.json'.format(dataType))
+coco_caps = COCO(captions_annFile)
+
+# get image ids
+ids = list(coco.anns.keys())
+
+
+
+# pick a random image and obtain the corresponding URL
+ann_id = np.random.choice(ids)
+img_id = coco.anns[ann_id]['image_id']
+print(img_id)
+img = coco.loadImgs(img_id)[0]
+url = img['coco_url']
+
+# print URL and visualize corresponding image
+print(url)
+I = io.imread(url)
+plt.axis('off')
+plt.imshow(I)
+plt.show()
+
+# load and display captions
+annIds = coco_caps.getAnnIds(imgIds=img['id']);
+anns = coco_caps.loadAnns(annIds)
+coco_caps.showAnns(anns)
